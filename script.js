9c98ba541661c387ee6ee5469c14a45c0ecb6b73
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-21 05:26:10 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your JS code here. If required.
 const line = document.getElementById("line");
+line.style.transform = rotate(90deg);
+
+setTimeout(function(){
+	line.style.transform = rotate(180deg);
+},500)
+
+setTimeout(function(){
+	line.style.transform = rotate(270deg);
+},800)
+
